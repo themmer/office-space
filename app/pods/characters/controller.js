f939865d711045e9467ef365c208b636651ddcc2
Tim Hemmer <themmer@gmail.com>
2016-02-18 23:28:17 -0600

    Added action callback

--- app/pods/characters/controller.js
+++ app/pods/characters/controller.js
@@ -1,16 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  selectCoverFlowCallback: Ember.computed(function() {
-    var _this = this;
-    return function(event, cover, index) {
-      let characters = _this.model;
-      let selectedCharacter = characters[index];
-      _this.set('selectedCharacter', selectedCharacter);
-    };
-  }),
 
   selectedCharacter: null,
 
-  selectedIndex: 1
+  actions: {
+    dataChangedAction(selectedData) {
+      console.log('yep', selectedData);
+    }
+  }
 });
--- app/pods/components/cover-flow/component.js
+++ app/pods/components/cover-flow/component.js
@@ -1,5 +1,23 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  classNames: ['coverflow']
+  classNames: ['coverflow'],
+
+  dataList: null,
+
+  changedDataAction: null,
+
+  selectCoverFlowCallback: Ember.computed(function() {
+    return (event, cover, index) => {
+      let dataList = this.get('dataList');
+      let selectedData = dataList[index];
+      this.set('selectedData', selectedData);
+      console.log('we are seeing a change in coverflow', selectedData);
+      this.get('dataChangedAction')(selectedData);
+    };
+  }),
+
+  selectedData: null,
+
+  selectedIndex: 1
 });
